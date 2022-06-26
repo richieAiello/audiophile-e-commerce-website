efd5bf8a180192ae3da2c348c98842f36c45c80e
Richard Aiello <richaiello17@gmail.com>
2022-06-26 10:36:36 -0400

    Added a logo that links to the home page to footer.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+import Logo from "jsx:../../assets/shared/desktop/logo.svg";
 import Facebook from "jsx:../../assets/shared/desktop/icon-facebook.svg";
 import Twitter from "jsx:../../assets/shared/desktop/icon-twitter.svg";
 import Instagram from "jsx:../../assets/shared/desktop/icon-instagram.svg";
@@ -6,6 +8,9 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__wrapper container">
+        <NavLink to="/" className="logo" aria-label="Return to top of home page.">
+          <Logo />
+        </NavLink>
         <div class="footer__wrapper--icons">
           <a href="#" className="footer__icon" aria-label="Visit audiophile's facebook.">
             <Facebook />
