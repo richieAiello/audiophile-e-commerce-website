79db3ca4b0d711ca30f79db3b1d02968936d1135
Richard Aiello <richaiello17@gmail.com>
2022-06-27 09:33:30 -0400

    Added and organized stating component and route files.

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import Logo from "jsx:../../assets/shared/desktop/logo.svg";
+import CartIcon from "jsx:../../assets/shared/desktop/icon-cart.svg";
+
+export default function Nav(props) {
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+
+      </nav>
+    </header>
+  );
+} 
